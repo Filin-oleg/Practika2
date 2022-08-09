05904306f9b038d465d592ced549a29ce4d19e64
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-09 21:55:49 +0200

    finish

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // цикл со счетчиком
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine(i + " ");
+// }
+
+
+// // Получить случайное число
+// int num = new Random().Next(0, 10);
+
+// //int  // целое число
+// //Float, double, decimal // дробные числа
+// //string // строка
+// //bool // логический тип. true false
+// //char // символ
+// //void // пустой тип
+
+// int[] array = new int[] {1, 2, 3, 4};
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = array[i] + 5;
+//     Console.WriteLine(array[i]);
+// } 
+
+// int[] array = new int[] {1, 2, 3, 4};
+// for (int i = array.Length - 1; i >= 0; i--)
+// {
+//     Console.WriteLine(array[i]);
+// } 
+
+string str = "abcd"; //  char[] str = new char[] {a, b, c, d};
+Console.WriteLine(str[2]);
